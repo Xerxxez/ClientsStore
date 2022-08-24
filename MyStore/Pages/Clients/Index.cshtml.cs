f5f80279f826b9c87c555746ec6128e315c5c5af
Xerxez <arian@DESKTOP-FTDN9V8>
2022-08-24 14:45:15 +0200

    klasse ausgelagert

--- MyStore/Pages/Clients/Index.cshtml.cs
+++ MyStore/Pages/Clients/Index.cshtml.cs
@@ -44,13 +44,5 @@ namespace MyStore.Pages.Clients
         }
     }
 
-    public class ClientInfo
-    {
-        public string Id { get; set; }
-        public string Name { get; set; }
-        public string Email { get; set; }
-        public string Phone { get; set; }
-        public string Address { get; set; }
-        public string CreatedAt { get; set; }   
-    }
+
 }
